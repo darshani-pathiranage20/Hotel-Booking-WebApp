ac5e8f625a054c73a3aaf914b33bde15b609bfdb
Darshani Pathiranage <it21161742@my.sliit.lk>
2025-12-16 02:43:34 +0530

    server,js change

--- server/server.js
+++ server/server.js
@@ -13,14 +13,16 @@ const app = express();
 app.use(cors()) // enable CORS for all routes
 
 
-//middleware 
-app.use(express.json()) // all request will be pass using the json methos
-app.use(clerkMiddleware())
-
 // API endpoint to listen to clerk webhooks
 // IMPORTANT: Webhook route MUST come BEFORE express.json()
 // Clerk needs raw body for signature verification
 app.post("/api/clerk", clerkWebhooks);
+
+//middleware 
+app.use(express.json()) // all request will be pass using the json methos
+app.use(clerkMiddleware())
+
+
 
 
 app.get('/', (req, res) => res.send("API is working, im good "))
